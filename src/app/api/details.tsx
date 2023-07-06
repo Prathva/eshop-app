d27ad5d2df83c4ef190285c32a78baff401d6c52
Prathva <parul.rathva@ajmerainfotech.com>
2023-07-06 12:50:43 +0530

    Created card Component

---
+++ src/app/api/details.tsx
@@ -0,0 +1,24 @@
+const cardData = [
+    {
+        name: "Docs",
+        url: "https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app",
+        desc: "Find in-depth information about Next.js features and API."
+    },
+    {
+        name: "Learn",
+        url: "https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app",
+        desc: "Learn about Next.js in an interactive course with quizzes!"
+    },
+    {
+        name: "Templates",
+        url: "https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app",
+        desc: "Explore the Next.js 13 playground."
+    },
+    {
+        name: "Deploy",
+        url: "https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app",
+        desc: "Instantly deploy your Next.js site to a shareable URL with Vercel."
+    }
+]
+
+export default cardData;
